896166876406ed2a900b3d401cbdcac5c0089e3c
arksnorman <arksnorman@gmail.com>
2017-04-15 21:16:42 +0300

    Refactor receipt

--- receipt.php
+++ receipt.php
@@ -1,20 +1,23 @@
 <?php
-$pageTitle = "Thank you for your order!";
-$section = "none";
-include("inc/header.php"); ?>
 
-	<div class="section page">
+	$pageTitle = "Thank you for your order!";
+	$section = "none";
+	include("inc/header.php"); 
 
-		<div class="wrapper">
+?>
 
-			<h1>Thank You!</h1>
+<div class="section page">
 
-			<p>Thank you for your payment. Your transaction has been completed, and a receipt for your purchase has been emailed to you. You may log into your account at <a href="http://www.paypal.com/us">www.paypal.com/us</a> to view details of this transaction.</p>
+	<div class="wrapper">
 
-			<p>Need another shirt already? Visit the <a href="shirts.php">Shirts Listing</a> page again.</p>
+		<h1>Thank You!</h1>
 
-		</div>
+		<p>Thank you for your payment. Your transaction has been completed, and a receipt for your purchase has been emailed to you. You may log into your account at <a href="http://www.paypal.com/us">www.paypal.com/us</a> to view details of this transaction.</p>
+
+		<p>Need another shirt already? Visit the <a href="/shirts.php">Shirts Listing</a> page again.</p>
 
 	</div>
+
+</div>
 
 <?php include("inc/footer.php"); ?>
