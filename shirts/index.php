deaa4b42694fd495b4ac35a839328ff8d44bed6f
arksnorman <arksnorman@gmail.com>
2017-04-16 18:56:09 +0300

    Refactoring shirts page

--- shirts/index.php
+++ shirts/index.php
@@ -8,23 +8,13 @@
 	require_once products;
 	require_once header;
 
+	echo "<h1>" . owner2 . " Full Catalog of SmartPhones</h1>";
+
+	foreach($products as $product_id => $product) 
+	{ 
+		echo get_list_view_html($product_id,$product);
+	}
+
+	require_once footer;
+
 ?>
-
-<div class="section shirts page">
-
-	<div class="wrapper">
-
-		<h1><?php echo owner2; ?> Full Catalog of SmartPhones</h1>
-
-		<ul class="products">
-			<?php foreach($products as $product_id => $product) { 
-					echo get_list_view_html($product_id,$product);
-				}
-			?>
-		</ul>
-
-	</div>
-
-</div>
-
-<?php require_once footer; ?>
