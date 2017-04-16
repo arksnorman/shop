d95ea206b3a46da6f5afb422e349b3478d97127b
arksnorman <arksnorman@gmail.com>
2017-04-16 03:22:39 +0300

    Added error messages to contact page

--- contact/index.php
+++ contact/index.php
@@ -7,30 +7,22 @@
 
 	if ($_SERVER["REQUEST_METHOD"] == "POST") 
 	{
-	    $name = trim($_POST["name"]);
-	    $email = trim($_POST["email"]);
-	    $message = trim($_POST["message"]);
+	    $name = htmlspecialchars(trim($_POST["name"]));
+	    $email = htmlspecialchars(trim($_POST["email"]));
+	    $message = htmlspecialchars(trim($_POST["message"]));
+	    $errorMessages = array();
 
-
-	    if ($name == "" OR $email == "" OR $message == "") 
+	    if ($name == "" OR $email == "" OR $message == "")
 	    {
-	        echo "You must specify a value for name, email address, and message.";
-	        exit;
+	       $errorMessages[] = "You must specify a value for name, email address, and message.";
 	    }
 
 	    foreach( $_POST as $value )
 	    {
 	        if( stripos($value,'Content-Type:') !== FALSE )
 	        {
-	            echo "There was a problem with the information you entered.";    
-	            exit;
+	            $errorMessages[] = "There was a problem with the information you entered."; 
 	        }
-	    }
-
-	    if ($_POST["address"] != "") 
-	    {
-	        echo "Your form submission has an error.";
-	        exit;
 	    }
 
 	    require_once phpMailer;
@@ -38,29 +30,33 @@
 
 	    if (!$mail->ValidateAddress($email))
 	    {
-	        echo "You must specify a valid email address.";
-	        exit;
+	        $errorMessages[] = "You must specify a valid email address.";	        
 	    }
 
-	    $email_body = "";
-	    $email_body = $email_body . "Name: " . $name . "<br>";
-	    $email_body = $email_body . "Email: " . $email . "<br>";
-	    $email_body = $email_body . "Message: " . $message;
-
-	    $mail->SetFrom($email, $name);
-	    $address = "orders@shirts4mike.com";
-	    $mail->AddAddress($address, "Shirts 4 Mike");
-	    $mail->Subject    = "Shirts 4 Mike Contact Form Submission | " . $name;
-	    $mail->MsgHTML($email_body);
-
-	    if(!$mail->Send()) 
+	    if (count($errorMessages) == 0) 
 	    {
-	      echo "There was a problem sending the email: " . $mail->ErrorInfo;
-	      exit;
-	    }
+		    $email_body = "";
+		    $email_body = $email_body . "Name: " . $name . "<br>";
+		    $email_body = $email_body . "Email: " . $email . "<br>";
+		    $email_body = $email_body . "Message: " . $message;
 
-	    header("Location: contact.php?status=thanks");
-	    exit;
+		    $mail->SetFrom($email, $name);
+		    $address = "orders@shirts4mike.com";
+		    $mail->AddAddress($address, "Shirts 4 Mike");
+		    $mail->Subject    = "Shirts 4 Mike Contact Form Submission | " . $name;
+		    $mail->MsgHTML($email_body);
+
+		    if($mail->Send()) 
+		    {
+		    	header("Location: contact.php?status=thanks");
+		    	exit;
+		    }
+		    else
+		    {
+		    	$errorMessages[] = "There was a problem sending the email: " . $mail->ErrorInfo;
+		    }
+		}
+	    
 	}
 
 	require_once header;
@@ -71,27 +67,39 @@
 <?php if (isset($_GET["status"]) AND $_GET["status"] == "thanks") { ?>
     <p>Thanks for the email! I&rsquo;ll be in touch shortly!</p>
 <?php } else { ?>
-<p class="text-center">I’d love to hear from you! Complete the form to send me an email.</p>
+
 <div class="row">
 	<div class="col-md-4"></div>
 	<div class="col-md-4">
-		<form action="contact.php" method="POST" role="form">
-			<legend>Contact</legend>
-		
+		<form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="POST" role="form">
+			<?php 
+			if (isset($errorMessages)) 
+			{				
+				foreach ($errorMessages as $errorMessage) 
+				{
+					echo '<div class="alert alert-danger">' . $errorMessage . '</div>';
+				}
+			}
+			else
+			{
+				echo '<p class="text-center">I’d love to hear from you! Complete the form to send me an email.</p>';
+			}
+			?>
 			<div class="form-group">
 				<label for="">Name</label>
-				<input type="name" class="form-control" id="" name="name" placeholder="Enter your name">
+				<input type="name" class="form-control" id="" name="name" value="<?php if(isset($name)){echo $name;}?>" placeholder="Enter your name">
 			</div>
 			<div class="form-group">
 				<label for="">Email</label>
-				<input type="email" class="form-control" id="" name="email" placeholder="Enter your email address">
+				<input type="email" class="form-control" id="" name="email" value="<?php if(isset($email)){echo $email;}?>" placeholder="Enter your email address">
 			</div>
 			<div class="form-group">
 				<label for="message">Message</label>
-				<textarea class="form-control" rows="3" id="comment"></textarea>
+				<textarea class="form-control" name="message" rows="3" id="comment"><?php 
+				if(isset($message)){echo $message;}?></textarea>
 			</div>
 
-			<button type="submit" class="btn btn-success">Send</button>
+			<button type="submit" name="submit" class="btn btn-success">Send</button>
 		</form>
 		<?php } ?>
 	</div>
