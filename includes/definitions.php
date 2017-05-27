b70cd45d4c6a9226dae41e1fa71aa8b287953e3e
arksnorman <arksnorman@gmail.com>
2017-05-28 00:15:52 +0300

    [includes/definitions.php] Changed Owner's Name

--- includes/definitions.php
+++ includes/definitions.php
@@ -1,13 +1,13 @@
 <?php
 
-	define('owner', "Lilian's Online Phone Shop");
-	define('owner2', "Lilian's");
-	define('owner3', 'Lilian');	
-	define('facebook', 'facebook.com/Lilian');
-	define('twitter', 'twitter.com/Lilian');
+	define('owner', "Arksnorman's Online Phone Shop");
+	define('owner2', "Arksnorman's");
+	define('owner3', 'Arksnorman');	
+	define('facebook', 'facebook.com/');
+	define('twitter', 'twitter.com/');
 	define('header', rootPath . '/includes/header.php');
 	define('footer', rootPath . '/includes/footer.php');
 	define('functions', rootPath . '/includes/functions.php');					
-	define('supportMail', 'lilian@lilianshop.proj');	
+	define('supportMail', 'arksnorman@arksnorman.tk');
 	
 ?>
