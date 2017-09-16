187d97723f64af6dcdf558b4826d91fcf6b48c9e
arksnorman <arksnorman@gmail.com>
2017-09-17 00:36:14 +0300

    Rewrite contact page

---
+++ templates/contact.php
@@ -0,0 +1,27 @@
+<br><br><br>
+<div class="row">
+    <div class="col-md-3"></div>
+    <div class="col-md-6">
+        <div class="alert alert-info" role="alert">
+            <strong>
+                <h3 class="text-center">Get in touch</h3>
+            </strong>
+        </div>
+        <form action="/contact/" method="POST" role="form">
+            <div class="form-group">
+                <label for="">Name</label>
+                <input type="name" class="form-control" name="name" placeholder="Enter your name">
+            </div>
+            <div class="form-group">
+                <label for="">Email</label>
+                <input type="email" class="form-control" name="email" placeholder="Enter your email address">
+            </div>
+            <div class="form-group">
+                <label for="message">Message</label>
+                <textarea class="form-control" name="message" rows="3" id="comment"></textarea>
+            </div>
+            <button type="submit" name="submit" class="btn btn-info float-right"><i class="fa fa-send"></i> Send</button>
+        </form>
+    </div>
+    <div class="col-md-3"></div>
+</div>
