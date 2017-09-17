46034403c692d45fd5d7b3a3527ab517d84e20e5
arksnorman <arksnorman@gmail.com>
2017-09-18 02:09:56 +0300

    Added forgotten receipt template

---
+++ templates/receipt.php
@@ -0,0 +1,12 @@
+<br><br><br>
+<div class="row">
+    <div class="col-md-2"></div>
+    <div class="col-md-8 text-center">
+        <div class="alert alert-success text-center" role="alert">
+            <h4 class="alert-heading">Well done!</h4>
+            <p>Aww yeah, Your transaction has been successfully completed, and a receipt for your transaction has been emailed to you.</p>
+            <p class="mb-0">Need another smartphone already? Visit the <a href="/phones/">SmartPhone Full Catalog</a> page again.</p>
+        </div>
+    </div>
+    <div class="col-md-2"></div>
+</div>
